6d4cbc5581ef6b61f0661150e1814f0e637164f1
Bondarev <alexbondarev1331@mail.ru>
2021-12-17 14:45:58 +0300

    fixed import in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
-import {askName} from '../src/cli.js';
+import askName from '../src/cli.js';
 
 // '/home/alex/Hexlet-Projects/frontend-project-lvl1/src/cli.js'
 
 console.log('Welcome to the Brain Games!');
 
 askName();
-
