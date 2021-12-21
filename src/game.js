6dedc24d9998efaebffece56330422f375af57f9
Bondarev <alexbondarev1331@mail.ru>
2021-12-21 03:19:57 +0300

    created game.js

---
+++ src/game.js
@@ -0,0 +1,46 @@
+import readlineSync from "readline-sync";
+
+import randomInteger from "./randomInteger.js";
+
+const game = () => {
+  console.log("Welcome to the Brain Games!");
+
+  const name = readlineSync.question("May I have your name? ");
+
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let counter = 0;
+  while (counter < 3) {
+    const integerFromOneToHundred = randomInteger(1, 100);
+    console.log("Question: " + integerFromOneToHundred);
+
+    const answer = readlineSync.question("Your answer: ");
+
+    let correctAnswer;
+    correctAnswer = integerFromOneToHundred % 2 === 0 ? "yes" : "no";
+
+    if (correctAnswer === answer) {
+      console.log("Correct!");
+      counter += 1;
+    } else {
+      console.log(
+        "'" +
+          answer +
+          "'" +
+          " is wrong answer ;(. Correct answer was " +
+          "'" +
+          correctAnswer +
+          "'."
+      );
+      console.log("Let's try again, " + name + "!");
+      return;
+    }
+  }
+  if ((counter = 3)) {
+    console.log("Congratulations, " + name + "!");
+  }
+};
+
+export default game;
