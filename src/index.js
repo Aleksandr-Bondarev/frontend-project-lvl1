329603fe85216592c0edf5fb74b1af8c6f7405ae
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-07-15 15:10:52 +0300

    refactoring, created game engine

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+import greetAndDescribe from './utils/greetAndDescribe.js';
+import askQuestion from './utils/askQuestion.js';
+import handleWinOrLose from './utils/handleWinOrLose.js';
+
+const gameRunner = (game) => {
+  let counter = 0;
+  const answersToWin = 3;
+  let name;
+
+  while (counter < answersToWin) {
+    const { gameDescripton, question, correctAnswer } = game();
+
+    if (counter === 0) name = greetAndDescribe(gameDescripton);
+
+    askQuestion(question);
+
+    let userAnswer = readlineSync.question('Your answer: ');
+    userAnswer = isNaN(Number(userAnswer)) ? userAnswer : Number(userAnswer);
+
+    counter += 1;
+
+    const youAreWinnig = handleWinOrLose(
+      correctAnswer,
+      userAnswer,
+      name,
+      counter,
+      answersToWin
+    );
+
+    if (!youAreWinnig) return;
+  }
+};
+
+export default gameRunner;
